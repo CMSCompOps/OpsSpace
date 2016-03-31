9b1d2d4a6e2c4492a031a9dc985237a9ba1ceec4
dabercro <dabercro@mit.edu>
2016-03-31 12:28:23 +0200

    Installer works on Python 2.6 now.

--- ToolBox/utils.py
+++ ToolBox/utils.py
@@ -1,6 +1,18 @@
 
+import logging
 import os
 import subprocess
+import sys
+
+#if sys.version_info.major == 2:
+#    if sys.version_info.minor == 6:
+#        from utils_2_6.py import *
+#    elif sys.version_info.minor == 7:
+#        from utils_2_7.py import *
+
+FORMAT = "%(module)s.%(funcName)s(%(lineno)s) => %(message)s (%(asctime)s)"
+DATEFMT = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(format=FORMAT, datefmt=DATEFMT, level=logging.DEBUG)
 
 
 def load_env(configs):
@@ -29,3 +41,20 @@ def load_env(configs):
     else:
         print('You passed an invalid argument type to utils.load_env()')
         exit()
+
+def append_to_file(file_name, lines):
+    """Appends lines to a file
+
+    @param file_name is the file to append to
+    @param lines is the list of lines to append to the file
+    """
+
+    if type(lines) is not list:
+        append_to_file(file_name,[lines])
+
+    else:
+        the_file = open(file_name,'a')
+        for line in lines:
+            the_file.write(line + '\n')
+
+        the_file.close()
--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python2.7
 
 """ @file install.py
 
@@ -9,20 +9,36 @@ File mostly intended to set up working environment for operator as a script.
 import os
 import urllib
 
-from ToolBox.utils import load_env
+from ToolBox.utils import load_env, append_to_file
 
 
 class Installer:
     """Class the holds the information for installing workspace"""
 
-    ValidPackages = ['SiteReadiness', 'TransferTeam', 'WmAgentScripts']
+    # List of valid packages that can be installed with this script
+    ValidPackages = [
+        'SiteReadiness',
+        'TransferTeam',
+        'WmAgentScripts'
+    ]
+
+    # List of profiles searched for in user's home to append to PYTHONPATH
+    possibleProfiles = [
+        '.bashrc',
+        '.bash_profile'
+    ]
+
+    # Default GitHub account containing packages that can be installed
     CentralGitHub = 'CMSCompOps'
+
+    # Location of the OpsSpace
     InstallDirectory = os.path.dirname(os.path.realpath(__file__))
-    apiBaseUrl = 'https://api.github.com/repos/'
+
+    # Remote repository location
     gitHubUrl = 'https://github.com/'
-    possibleProfiles = ['.bashrc', '.bash_profile']
 
     def __init__(self, github_user):
+        """Initialize with the GitHub username of the operator."""
         self.UserName = github_user or self.CentralGitHub
 
     def print_valid_packages(self):
@@ -39,39 +55,67 @@ class Installer:
         user = self.UserName
         installed = False
 
-        while not installed:
-            check = urllib.urlopen(self.apiBaseUrl + user + '/' + package_name)
-            if check.getcode() == 404:
+        # Location to install the package
+        target_install = self.InstallDirectory + '/' + package_name
 
-                if user == self.CentralGitHub:
-                    error_string = 'Cannot find package ' + package_name
-                    if user != self.UserName:
-                        error_string += ' in ' + self.UserName + ' or'
-                    error_string += ' in ' + user + ' repository! Check again for package location.'
-                    print(error_string)
-                    exit(1)
+        if not os.path.exists(target_install):
+            while not installed:
+                # First pass will look at operator username, then the central repository
+                check = urllib.urlopen(self.gitHubUrl + user + '/' + package_name)
 
+                if check.getcode() == 404:
+
+                    # If checking central repository and getting 404, something is wrong
+                    if user == self.CentralGitHub:
+                        error_string = 'Cannot find package ' + package_name
+                        if user != self.UserName:
+                            error_string += ' in ' + self.UserName + ' or'
+                        error_string += ' in ' + user + ' repository! Check again for package location.'
+                        print(error_string)
+                        exit(1)
+
+                    # If not checking central repository, check that next
+                    else:
+                        user = self.CentralGitHub
+
+                # If repository exists, clone it
                 else:
-                    user = self.CentralGitHub
 
-            else:
-                command = 'git clone ' + self.gitHubUrl + user + '/' + package_name + '.git ' + \
-                          self.InstallDirectory + '/' + package_name
-                print(command)
-                os.system(command)
-                if user != self.CentralGitHub:
-                    to_write = [
-                        '[remote "' + self.CentralGitHub + '"]',
-                        '\turl = ' + self.gitHubUrl + self.CentralGitHub + '/' + package_name + '.git',
-                        '\tfetch = +refs/heads/*:refs/remotes/' + self.CentralGitHub + '/*'
-                    ]
-                    git_config = open(self.InstallDirectory + '/' + package_name + '/.git/config', 'a')
-                    for line in to_write:
-                        git_config.write(line + '\n')
+                    # Clone the repository inside the OpsSpace package
+                    command = 'git clone ' + self.gitHubUrl + user + '/' + package_name + '.git ' + target_install
+                    print(command)
+                    os.system(command)
 
-                    git_config.close()
+                    # If operator username, add the central location to remotes
+                    if user != self.CentralGitHub:
+                        # Lines to write to config
+                        to_write = [
+                            '[remote "' + self.CentralGitHub + '"]',
+                            '\turl = ' + self.gitHubUrl + self.CentralGitHub + '/' + package_name + '.git',
+                            '\tfetch = +refs/heads/*:refs/remotes/' + self.CentralGitHub + '/*'
+                        ]
 
-                installed = True
+                        # Append to the config file
+                        append_to_file(self.InstallDirectory + '/' + package_name + '/.git/config', to_write)
+
+                    installed = True
+        else:
+            print(package_name + ' already installed at ' + target_install + '!')
+
+        # Check the .gitignore for the package name and add it, if needed
+        # .gitignore location
+        git_ignore = self.InstallDirectory + '/.gitignore'
+        # Line to write to .gitignore
+        git_ignore_line = package_name + '/*'
+
+        # Get contents
+        git_ignore_file = open(git_ignore, 'r')
+        git_ignore_list = git_ignore_file.readlines()
+        git_ignore_file.close()
+
+        # Check for line
+        if git_ignore_line + '\n' not in git_ignore_list:
+            append_to_file(git_ignore, git_ignore_line)
 
     def install_packages(self, package_list):
         """Calls install_package for a list of packages"""
@@ -84,42 +128,69 @@ class Installer:
                 print(package + ' is an invalid package!!!')
                 has_invalid_package = True
 
+        # If user tried to install any invalid package, give list of valid packages
         if has_invalid_package:
             self.print_valid_packages()
 
     def add_pythonpath(self):
         """Appends modified $PYTHONPATH variable to bash profile"""
 
+        # Location to add to PYTHONPATH
         target_dir = '/'.join(self.InstallDirectory.split('/')[:-1])
 
         for profile_name in self.possibleProfiles:
             profile_path = os.environ.get('HOME') + '/' + profile_name
+            # Search for profile files
             if os.path.exists(profile_path):
+                # If there, source the profile file to get most updated variables
                 load_env(profile_path)
+                # If this directory is not in PYTHON path, append to profile
                 if target_dir not in os.environ.get('PYTHONPATH').split(':'):
-                    profile_file = open(profile_path, 'a')
-                    profile_file.write('\n# Python objects in CMSCompOps workspace\n')
-                    profile_file.write('export PYTHONPATH=$PYTHONPATH:' + target_dir + '\n')
-                    profile_file.close()
+                    append_to_file(profile_path, ['', '# Python objects in CMSCompOps workspace',
+                                                  'export PYTHONPATH=$PYTHONPATH:' + target_dir])
 
 
 def main():
     """Main functionality of the install script"""
 
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-    parser.add_argument('--UserName', '-u', metavar='UserName', dest='gitUser', default=os.environ.get('USER'),
-                        help='GitHub user name, where the packages will be searched for first (default ' +
-                             os.environ.get('USER') + ')')
-    parser.add_argument('packages', metavar='PACKAGES', nargs='*',
-                        help='List of valid packages to be installed. Giving no package will show list of possible ' +
-                             'packages to install')
+    packages = []
+    git_user = ''
 
-    args = parser.parse_args()
+    # If argparse available, use it
+    try:
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        parser.add_argument('--UserName', '-u', metavar='UserName', dest='gitUser', default=os.environ.get('USER'),
+                            help='GitHub user name, where the packages will be searched for first (default ' +
+                            os.environ.get('USER') + ')')
+        parser.add_argument('packages', metavar='PACKAGES', nargs='*',
+                            help='List of valid packages to be installed. ' +
+                                 'Giving no package will show list of possible ' +
+                                 'packages to install')
 
-    installer = Installer(args.gitUser)
+        args = parser.parse_args()
+        packages = args.packages
+        git_user = args.gitUser
 
-    if len(args.packages) == 0:
+    # Otherwise, use optparser
+    except ImportError:
+        from optparse import OptionParser
+        parser = OptionParser()
+        parser.add_option('--UserName', '-u', metavar='UserName', dest='gitUser', default=os.environ.get('USER'),
+                          help='GitHub user name, where the packages will be searched for first (default ' +
+                          os.environ.get('USER') + ')')
+        parser.add_option('-p', '--Packages', metavar = 'PackageNames', dest = 'packages', default = '',
+                          help = 'List of packages to install. Separated by commas, no spaces. ' +
+                                 'Examples:  -p package1,package2 or --Packages=package1,package2')
+
+        (options, args) = parser.parse_args()
+        packages = options.packages.split(',')
+        git_user = options.gitUser
+
+
+    installer = Installer(git_user)
+
+    if len(packages) == 0 or packages[0] == '':
         parser.print_help()
         installer.print_valid_packages()
     else:
@@ -128,7 +199,7 @@ def main():
             'I will now search for packages in ' + installer.UserName + '\'s repositories. ' +
             'If not found, will fall back on ' + installer.CentralGitHub + '.\n'
         )
-        installer.install_packages(args.packages)
+        installer.install_packages(packages)
 
     installer.add_pythonpath()
 
