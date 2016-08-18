7459a97e69a8b98858797bd1a95b953e77205824
dabercro <dabercro@mit.edu>
2016-08-18 23:36:23 +0200

    Cleaned bootstrap_tests.sh with a function.

--- test/bootstrap_test.sh
+++ test/bootstrap_test.sh
@@ -5,11 +5,40 @@
 
 if [ "$TRAVIS" != "true" ]
 then
-    echo "This script is designed to be run by travis-ci.org"
-    echo "for package tests."
+    echo "This script is designed to be run by travis-ci.org for package tests."
     echo "There should be no need to run it interactively."
     exit 1
 fi
+
+# Function to do the test for us
+_do_test () {
+
+
+    errors=$1                             # Track how many errors found so far
+    thetest="$2 $3"                       # Setup the test to be done
+
+    $packagetest                          # Do the test
+    errorcode=$?                          # Get the error code
+
+    if [ $errorcode -ne 0 ]               # If failed tell user, and increase error count
+    then
+
+        tput setaf 1 2> /dev/null
+        echo "$packagetest exited with error code $errorcode!"
+        errors=`expr $errors + 1`
+
+    else                                  # If passed, tell for fun too
+
+        tput setaf 2 2> /dev/null
+        echo "$packagetest passed!"
+        
+    fi
+
+    tput sgr0 2> /dev/null                # Reset colors
+
+    return $errors                        # Return total number of errors
+
+}
 
 export HOME=`pwd`                         # Get the package you are testing from location and change HOME
 package=${HOME##*/}
@@ -27,26 +56,14 @@ packagetest=$package/test/run_tests.sh
 if [ -f $packagetest ]                    # If the default test script exists, run it
 then
 
-    $packagetest
-    errorcode=$?
+    _do_test $ERRORSFOUND $packagetest
+    ERRORSFOUND=$?
 
-    if [ $errorcode -ne 0 ]
-    then
-        tput setaf 1 2> /dev/null
-        echo "$packagetest exited with error code $errorcode."
-        ERRORSFOUND=`expr $ERRORSFOUND + 1`
-    fi
 fi
 
-test/run_tests.sh $package                # Run main test, with this package required to succeed
-errorcode=$?
-
-if [ $errorcode -ne 0 ]
-then
-    tput setaf 1 2> /dev/null
-    echo "test/run_tests.sh $package exited with error code $errorcode."
-    ERRORSFOUND=`expr $ERRORSFOUND + 1`
-fi
+# Run main test, with this package required to succeed
+_do_test $ERRORSFOUND test/run_tests.sh $package
+ERRORSFOUND=$?
 
 if [ $ERRORSFOUND -eq 1 ]                 # Setting plural correctly
 then                                      #   looks impressive to some
