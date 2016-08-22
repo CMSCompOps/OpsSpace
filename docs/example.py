3d5e31990100cacba520911ce4c8c802a775c820
dabercro <dabercro@mit.edu>
2016-08-22 12:58:33 +0200

    Finished developer guidelines for now.

--- docs/example.py
+++ docs/example.py
@@ -5,13 +5,18 @@
 
 def example_function(an_integer, a_string, int_or_str):
     """My example function for the developer guidelines.
-    
+
     :param int an_integer: This is a description for what an integer can be used for
     :param str a_string: This is a description for what a string can be used for
-    :param int_or_string: This is just an example for more complex variables types
-    :type int_or_string: int or str
+    :param int_or_str: This is just an example for more complex variables types
+    :type int_or_str: int or str
     :returns: The number 0
     :rtype: int
+    :raises TypeError: If any of the variables are the wrong type
     """
-      
+
+    if not (isinstance(an_integer, int) and isinstance(a_string, str)
+            and (isinstance(int_or_str, int) or isinstance(int_or_str, str))):
+        raise TypeError('One of the input variables are of the wrong type.')
+
     return 0
