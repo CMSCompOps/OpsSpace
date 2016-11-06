2f9440a5db40e0348ef0e2fe268b3772ac63c4e0
dabercro <dabercro@mit.edu>
2016-11-06 18:03:49 +0100

    Added support for Perl documentation style.

--- docs/customdocs.py
+++ docs/customdocs.py
@@ -1,13 +1,13 @@
 from sphinxcontrib.autoanysrc import analyzers
 
-class ShellScriptAnalyzer(analyzers.BaseAnalyzer):
+class CustomAnalyzer(analyzers.BaseAnalyzer):
     """
-    Processes shell scripts
+    Simple class that takes arbitrary files, starts and ends blocks, and strips comment characters
+    """
 
-    Comment blocks start and end with ##!.
-    Assumes the comments start with # and a space, so
-    it just returns the line with the first two characters stripped.
-    """
+    start_block = '"""'  # String to start a comment block
+    end_block = '"""'    # String to end a comment block
+    strip_leading = 0    # Number of characters to strip from the beginning of the comments
 
     def process(self, content):
 
@@ -16,18 +16,41 @@ class ShellScriptAnalyzer(analyzers.BaseAnalyzer):
         for lineno, srcline in enumerate(content.split('\n')):
 
             line = srcline.rstrip()
-            if line.startswith('##!'):
-                if in_comment_block:
+            if in_comment_block:
+                if line.startswith(self.end_block):
                     in_comment_block = False
+                    continue
+
+                if len(line) < self.strip_leading:
+                    yield '', lineno
                 else:
-                    in_comment_block = True
-                continue
+                    yield line[self.strip_leading:], lineno
 
-            if not in_comment_block:
-                continue
+            elif line.startswith(self.start_block):
+                in_comment_block = True
 
-            if len(line) < 3:
-                yield '', lineno
 
-            else:
-                yield line[2:], lineno
+
+class ShellScriptAnalyzer(CustomAnalyzer):
+    """
+    Processes shell scripts
+
+    Comment blocks start and end with ##!.
+    Assumes the comments start with # and a space, so
+    it just returns the line with the first two characters stripped.
+    """
+
+    start_block = '##!'
+    end_block = '##!'
+    strip_leading = 2
+
+
+class PODAnalyzer(CustomAnalyzer):
+    """
+    Processes perl scripts with POD comments
+
+    Comment blocks start with =pod and end with =cut.
+    """
+
+    start_block = '=pod'
+    end_block = '=cut'
