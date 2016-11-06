0e1a68e50f93b42caaac85422404fb9b9b6df0cf
dabercro <dabercro@mit.edu>
2016-11-06 22:33:22 +0100

    Change headers to Sphinx style headers.

--- docs/customdocs.py
+++ docs/customdocs.py
@@ -5,9 +5,12 @@ class CustomAnalyzer(analyzers.BaseAnalyzer):
     Simple class that takes arbitrary files, starts and ends blocks, and strips comment characters
     """
 
-    start_block = '"""'  # String to start a comment block
-    end_block = '"""'    # String to end a comment block
-    strip_leading = 0    # Number of characters to strip from the beginning of the comments
+    comment_starts_with = '"""'  # String to start a comment block
+    comment_ends_with = '"""'    # String to end a comment block
+    strip_leading = 0            # Number of characters to strip from the beginning of the comments
+
+    def process_line(self, line):
+        yield line
 
     def process(self, content):
 
@@ -17,16 +20,18 @@ class CustomAnalyzer(analyzers.BaseAnalyzer):
 
             line = srcline.rstrip()
             if in_comment_block:
-                if line.startswith(self.end_block):
+                if line.startswith(self.comment_ends_with):
                     in_comment_block = False
+                    yield '', lineno
                     continue
 
                 if len(line) < self.strip_leading:
                     yield '', lineno
                 else:
-                    yield line[self.strip_leading:], lineno
+                    for parsed in self.process_line(line[self.strip_leading:]):
+                        yield parsed, lineno
 
-            elif line.startswith(self.start_block):
+            elif line.startswith(self.comment_starts_with):
                 in_comment_block = True
 
 
@@ -40,8 +45,8 @@ class ShellScriptAnalyzer(CustomAnalyzer):
     it just returns the line with the first two characters stripped.
     """
 
-    start_block = '##!'
-    end_block = '##!'
+    comment_starts_with = '##!'
+    comment_ends_with = '##!'
     strip_leading = 2
 
 
@@ -52,5 +57,15 @@ class PODAnalyzer(CustomAnalyzer):
     Comment blocks start with =pod and end with =cut.
     """
 
-    start_block = '=pod'
-    end_block = '=cut'
+    comment_starts_with = '=pod'
+    comment_ends_with = '=cut'
+
+    headers = ['=', '~', '+']
+
+    def process_line(self, line):
+        if line.startswith('=head'):
+            output = ' '.join(line.split()[1:])
+            for final in [output, self.headers[int(line[5]) - 1] * len(output)]:
+                yield final
+        else:
+            yield line
