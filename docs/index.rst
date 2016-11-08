89ce0eb9b428a3fc165ce2c1289e82e19fcb7a98
dabercro <dabercro@mit.edu>
2016-11-08 18:40:55 +0100

    Added dbs client package to installation.

--- docs/index.rst
+++ docs/index.rst
@@ -11,6 +11,7 @@ Contents:
    setup
    developer
    toolbox
+   dbs
    workflowwebtools
    siteadmintoolkit
    todo
--- install.sh
+++ install.sh
@@ -5,15 +5,18 @@
 if [ ! -d dbs_src ]
 then
 
-    git clone https://github.com/dmwm/DBS.git dbs_src
+    git clone --depth=1 https://github.com/dmwm/DBS.git dbs_src
+    cd dbs_src || exit 1
+    git checkout DBS_3_3_150
+    cd .. || exit 1
 
-else
+    mv dbs_src/PycurlClient/src/python/RestClient RestClient
+    mv dbs_src/Client/src/python/dbs dbs
 
-    cd dbs_src
-    git pull
-    cd ..
+    # I don't use this doc at the moment
+    rm -rf dbs_src/doc
+
+    # Patch dbsClient for documentation
+    patch dbs/apis/dbsClient.py patch_dbs.txt
 
 fi
-
-mv dbs_src/Client/src/python/dbs dbs
-mv dbs_src/PycurlClient/src/python/RestClient RestClient
