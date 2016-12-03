9e6f7d6959b08aa6fa6de39016335a946720d87f
dabercro <dabercro@mit.edu>
2016-12-03 16:37:19 +0100

    Trying to add js tests.

---
+++ test/test_js_style.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+
+# Check for eslint
+
+if [ "$(which eslint 2> /dev/null)" = "" ]
+then
+
+    echo "Please install eslint: https://github.com/eslint/eslint" 1>&2
+    exit 0
+
+fi
+
+# Save here, in case user is not in test dir
+here=$(pwd)
+
+# Get the test dir
+testdir=${0%%$(basename "$0")}
+cd "$testdir" || exit 1
+testdir=$(pwd)
+
+# Set text output location
+outputdir=$testdir"/eslint_output"
+
+test -d "$outputdir" || mkdir "$outputdir"
+
+cd ..
+
+ERRORSFOUND=0
+
+# Define the function to check each repository
+check_package () {
+
+    location="$1"
+
+    eslint -c test/eslintrc.yml "$location" > "$outputdir/$location.txt"
+
+    if [ $? -eq 0 ]
+    then
+        tput setaf 2 2> /dev/null
+        echo "$outputdir/$location.txt passed the check."
+        tput sgr0 2> /dev/null
+        return
+    fi
+
+    tput setaf 1 2> /dev/null
+    echo "$outputdir/$location.txt failed the check."
+    tput sgr0 2> /dev/null
+
+    if [ "$TRAVIS" != "true" ] || [ "$location" = "$MUSTWORK" ]
+    then
+
+        ERRORSFOUND=$((ERRORSFOUND + 1))
+
+    fi
+
+    if [ "$TRAVIS" = "true" ]
+    then
+
+        cat "$outputdir/$location.txt"
+
+    fi
+
+}
+
+# Check each installed package
+while read -r package
+do
+
+    if [ -d "$package" ]
+    then
+
+        check_package "$package"
+
+    fi
+
+done < PackageList.txt
+
+cd "$here" || exit 1
+
+echo "$ERRORSFOUND errors found"
+
+exit "$ERRORSFOUND"
