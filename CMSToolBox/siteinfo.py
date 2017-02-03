2bd74a6b7b859b27e6af75d014c353736d8ae4f3
dabercro <daniel.r.abercrombie@gmail.com>
2017-02-03 13:26:04 -0500

    Added Wisconsin and typo in setup.rst.

--- CMSToolBox/siteinfo.py
+++ CMSToolBox/siteinfo.py
@@ -41,18 +41,19 @@ def get_domain(site):
     """
 
     host_map = {
-        'T2_AT_Vienna':   'oeaw.ac.at',
-        'T2_BE_IIHE':     'iihe.ac.be',
-        'T2_BE_UCL':      'ucl.ac.be',
-        'T2_BR_SPRACE':   'sprace.org.br',
-        'T2_CH_CSCS':     'cscs.ch',
-        'T2_DE_DESY':     'desy.de',
-        'T2_ES_CIEMAT':   'ciemat.es',
-        'T2_US_Caltech':  'ultralight.org',
-        'T2_US_Florida':  'ufl.edu',
-        'T2_US_MIT':      'mit.edu',
-        'T2_US_Nebraska': 'unl.edu',
-        'T2_US_UCSD':     'ucsd.edu',
+        'T2_AT_Vienna':    'oeaw.ac.at',
+        'T2_BE_IIHE':      'iihe.ac.be',
+        'T2_BE_UCL':       'ucl.ac.be',
+        'T2_BR_SPRACE':    'sprace.org.br',
+        'T2_CH_CSCS':      'cscs.ch',
+        'T2_DE_DESY':      'desy.de',
+        'T2_ES_CIEMAT':    'ciemat.es',
+        'T2_US_Caltech':   'ultralight.org',
+        'T2_US_Florida':   'ufl.edu',
+        'T2_US_MIT':       'mit.edu',
+        'T2_US_Nebraska':  'unl.edu',
+        'T2_US_Wisconsin': 'wisc.edu',
+        'T2_US_UCSD':      'ucsd.edu',
         }
 
     return host_map[site]
