b6c832ab7432ce7a1abb06b7345582a27e372cfe
dabercro <daniel.r.abercrombie@gmail.com>
2017-03-07 10:55:38 -0500

    Also add drain status to sitereadiness module.

--- CMSToolBox/sitereadiness.py
+++ CMSToolBox/sitereadiness.py
@@ -15,12 +15,12 @@ from .dashboard import GLOBAL_CACHE
 def i_site_readiness():
     """Iterates over site readiness for the user
 
-    :returns: iterator tuple with site, readiness status
+    :returns: iterator tuple with site, readiness, and drain status
     """
     info = GLOBAL_CACHE.get('ssb_237')
 
     for site_info in info:
-        yield site_info['VOName'], site_info['COLORNAME']
+        yield site_info['VOName'], site_info['COLORNAME'], site_info['Status']
 
 
 def site_list():
@@ -32,7 +32,7 @@ def site_list():
 
     output = []
 
-    for site, _ in i_site_readiness():
+    for site, _, _ in i_site_readiness():
         output.append(site)
 
     output.sort()
@@ -52,7 +52,27 @@ def site_readiness(site_name):
 
     :rtype: str
     """
-    for site, output in i_site_readiness():
+    for site, output, _ in i_site_readiness():
+        if site == site_name:
+            return output
+
+    return 'none'
+
+
+def site_drain_status(site_name):
+    """Returns the drain status for a given site
+
+    :param str site_name: Name of the site
+    :returns: Readiness status. Possibilities and their meanings are:
+
+              - 'enabled': The site is running
+              - 'drain': The jobs at the site are being drained
+              - 'test': This site is being commissioned or something?
+              - 'none': Site not found
+
+    :rtype: str
+    """
+    for site, _, output in i_site_readiness():
         if site == site_name:
             return output
 
