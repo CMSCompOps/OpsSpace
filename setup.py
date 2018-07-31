d41fcf95ccfa2c0212c6d0464a8ebd220bf7936a
dabercro <dabercro@mit.edu>
2018-07-31 19:24:45 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='cmstoolbox',
-    version='0.9.1',
+    version='0.9.2',
     packages=setuptools.find_packages(),
     author='Daniel Abercrombie',
     author_email='dabercro@mit.edu',
